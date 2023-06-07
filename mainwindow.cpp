82c75ad76859da9567d864c1302fe50429338a7b
Андрей Ужегов <sg6481441@gmail.com>
2023-06-08 01:53:52 +0300

    advanced

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -6,10 +6,18 @@ MainWindow::MainWindow(QWidget *parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    ui->centralwidget->setLayout(ui->MainLayout);
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+
+void MainWindow::on_connectButton_clicked()
+{
+    ui->IPlabel->hide();
 }
 
